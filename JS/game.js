4d66d4101679185b1334860eadc42e060aefb720
connor <connor.white.spam@gmail.com>
2019-08-09 14:56:14 +1000

    making progress towards progen (NOT WORKING)

--- JS/game.js
+++ JS/game.js
@@ -1,51 +1,60 @@
 // config variables
-var globalpos = [0,0]
-var map = {};
-var size = 512
-var sps = 11
-var playerpos = [(sps-1)/2, (sps-1)/2]
+let globalpos = [0,0]
+const map = {};
+const size = 512
+let genmap;
+const sps = 11
+const playerpos = [(sps-1)/2, (sps-1)/2]
 map[(sps-1)/2+","+(sps-1)/2] = {"type":"grass", "stand":"True", "special": "none", "enemy": "none"}
-var maptext = $.ajax({
+const maptext = $.ajax({
     type: "GET",
     url: "/map",
     async: false
 }).responseText.split("\n");
 for (i = 0; i < maptext.length; i++){
-    var tile = maptext[i].split("|")
+    let tile = maptext[i].split("|")
     map[tile[0]+","+tile[1]] = {"type":tile[2], "stand":tile[3], "special": tile[4], "enemy": tile[5]}
 };
-var canvas = document.getElementById("screen");
-var ctx = canvas.getContext("2d");
+const canvas = document.getElementById("screen");
+const ctx = canvas.getContext("2d");
 
-
+const canvas2 = document.getElementById("screen2");
+const ctx2 = canvas2.getContext("2d");
 
 window.onload = function() {
     window.addEventListener("keypress", update);
+    genmap = $.ajax({
+        type: "GET",
+        url: "/snoise?size=1000",
+        async: false
+    }).responseJSON
+    console.log(genmap);
     drawscreen(0,0)
+
 };
 
 //drawing the screen
-var drawscreen = (movex,movey) => {
-    var terrain = {
+const drawscreen = (movex,movey) => {
+    const terrain = {
         "grass": {"colour":"#33cc33", "stand":"True", "image":"False", "height":"1"},
         "water": {"colour":"#0033cc", "stand":"False", "image":"False", "height":"3"},
         "mountain": {"colour":"#666633", "stand":"True", "image":"False", "height":"4"},
         "lava": {"colour":"#cc6600", "stand":"False", "image":"False", "height":"5"},
         "forest": {"colour":"#336600", "stand":"True", "image":"False", "height":"2"},
     };
-    var entities = {
+    const entities = {
         "player": {"colour":"#0d0d0d"},
         "goblin": {"colour":"#ff0000"}
     }
-    var interest = ["fountain", "dungeon", "monster", "teleport"]
-    var draw = (x,y) => {
+    const interest = ["fountain", "dungeon", "monster", "teleport"]
+    const draw = (x,y) => {
         // check to see if the tile changes (although redraws if different interest currently)
         if (map[(x+globalpos[0]).toString()+","+(y+globalpos[1]).toString()] != map[(x+globalpos[0]-movex).toString()+","+(y+globalpos[1]-movey).toString()] || movex+movey == 0){
             if(terrain[map[(x+globalpos[0]).toString()+","+(y+globalpos[1]).toString()]['type']]["image"]=="False") {
                 ctx.fillStyle = terrain[map[(x+globalpos[0]).toString()+","+(y+globalpos[1]).toString()]['type']]["colour"];
                 ctx.fillRect((x)*(size/sps), (y)*(size/sps), (size/sps), (size/sps))
             } else {
-                var img = document.getElementById(map[(x+globalpos[0]).toString()+","+(y+globalpos[1]).toString()]['type']);
+                let img = document.getElementById(map[(x+globalpos[0]).toString()+","+(y+globalpos[1]).toString()]['type']);
                 ctx.drawImage(img, (x)*(size/sps), (y)*(size/sps));
             }
 
@@ -62,8 +71,10 @@ var drawscreen = (movex,movey) => {
                 draw(x,y)
             } catch (e) { // if the tile dosent exist yet
                 if (e instanceof TypeError ){
-
-                    if(Math.random() <= 0.6){ // terrain
+                    console.log(x+globalpos[0]+500, y+globalpos[1]+500)
+                    console.log(genmap[x+globalpos[0]+500][y+globalpos[1]+500]);
+                    // chances of different tiles
+                    if(genmap[(x+globalpos[0]+500)][(y+globalpos[1]+500)] <= 0.6 && genmap[(x+globalpos[0]+500)][(y+globalpos[1]+500)] >= 0.4){ // terrain
                         type = "grass"
                         stand = "True"
                         if(Math.random() <= 0.01){ // enemies
@@ -72,7 +83,28 @@ var drawscreen = (movex,movey) => {
                         } else {
                             enemy = "none"
                         }
-                    }else{
+                    }
+                    else if (genmap[(x+globalpos[0]+500)][(y+globalpos[1]+500)] < 0.4 && genmap[(x+globalpos[0]+500)][(y+globalpos[1]+500)] >= 0.2) {
+                        type = "forest"
+                        stand = "True"
+                        enemy = "none"
+                    }
+                    else if (genmap[(x+globalpos[0]+500)][(y+globalpos[1]+500)] > 0.6 && genmap[(x+globalpos[0]+500)][(y+globalpos[1]+500)] <= 0.8) {
+                        type = "mountain"
+                        stand = "True"
+                        enemy = "none"
+                    }
+                    else if (genmap[(x+globalpos[0]+500)][(y+globalpos[1]+500)] < 0.2) {
+                        type = "water"
+                        stand = "False"
+                        enemy = "none"
+                    }
+                    else if (genmap[(x+globalpos[0]+500)][(y+globalpos[1]+500)] > 0.8) {
+                        type = "lava"
+                        stand = "False"
+                        enemy = "none"
+                    }
+                    else{
                         type = Object.keys(terrain)[Object.keys(terrain).length * Math.random() << 0]
                         stand = terrain[type]["stand"]
                         enemy = "none"
@@ -98,8 +130,8 @@ var drawscreen = (movex,movey) => {
 }
 
 function update(key) { //keys
-    var movex = 0;
-    var movey = 0;
+    let movex = 0;
+    let movey = 0;
     if(key["code"] == "KeyW"){
         movey = -1;
     }
@@ -112,19 +144,9 @@ function update(key) { //keys
     else if (key["code"] == "KeyD"){
         movex = 1;
     }
-    if (movex+movey != 0){// && playerpos[0] + movex >=0 && playerpos[0] + movex <sps && playerpos[1] + movey >=0 && playerpos[1] + movey <sps){
-    //
-    //     if (map[(playerpos[0]+movex)+","+(playerpos[1]+movey)]['stand'] == "True"){
-    //         ctx.fillStyle = terrain[map[playerpos[0]+","+playerpos[1]]['type']];
-    //         ctx.fillRect(playerpos[0]*(size/sps)+16, playerpos[1]*(size/sps)+16, 32, 32);
-    //         playerpos[0] += movex;
-    //         playerpos[1] += movey;
-    //         ctx.fillStyle = terrain["player"]
-    //         ctx.fillRect(playerpos[0]*(size/sps)+16, playerpos[1]*(size/sps)+16, 32, 32);
-    //     }
+    if (movex+movey != 0){
         if (map[(playerpos[0]+globalpos[0]+movex)+","+(playerpos[1]+globalpos[1]+movey)]['stand'] == "True"){
             globalpos = [globalpos[0]+movex, globalpos[1]+movey]
-            console.log(globalpos);
             drawscreen(movex,movey);
         }
     }
--- JS/server.js
+++ JS/server.js
@@ -9,7 +9,6 @@ app.get('/map', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-    const page = fs.readFileSync("index.html", 'utf8');
     res.sendfile("index.html");
 });
 app.get('/js', function(req, res) {
@@ -25,8 +24,61 @@ app.get('/noisejs', function(req, res) {
     res.send(page);
 });
 
+
 app.get('/snoise', function(req, res) {
-    res.send(new SimplexNoise(););
+    let size = req.query.size;
+    let img3 = []
+    for(i = 0; i < size*size/50; ++i){
+        gen = new SimplexNoise()
+        const img = genee(size/50, size/50)
+        const img2 = map(img,(cur, px,py,ix,iy)=>{
+                let v = octave_noise(ix,iy,8)
+                return v
+        })
+        for(lis = 0; lis < img2.length; ++lis){
+            img3.push(img2[lis])
+        }
+        console.log(img3);
+    }
+
+    function genee(width, height) {
+            const rows = [];
+            for (let y = 0; y < height; y++) {
+                const row = [];
+                for(let x=0; x<width; x++) {
+                    row[x] = 0;
+                }
+                rows[y] = row;
+            }
+            return rows;
+    }
+
+    function octave_noise(nx,ny,octaves) {
+        let val = 0;
+        let freq = 1;
+        let max = 0;
+        let amp = 1;
+        for(let i=0; i<octaves; i++) {
+            gene = gen.noise2D(nx, ny) * 0.5 + 0.5;
+            val += parseFloat(gene)*amp;
+            max += amp;
+            amp /= 2;
+            freq  *= 2;
+        }
+        return val/max;
+    }
+
+    function map(data,cb) {
+        const width = data[0].length;
+        const height = data.length;
+        return data.map((row,py)=>{
+            return row.map((val,px) => {
+                const nx = px/width, ny = py/height;
+                return cb(val,px,py,nx,ny);
+            })
+        });
+    }
+    res.json(img3);
 });
 
 app.listen(3000, function() {
