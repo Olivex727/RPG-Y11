b4a62878243bdc27e06e9b148871b445c8b7189f
connor <connor.white.spam@gmail.com>
2019-08-12 15:53:22 +1000

    added a cashed map for faster load

--- JS/server.js
+++ JS/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require("fs");
 const SimplexNoise = require('simplex-noise');
 const app = express();
+let cashedmap = []
 
 app.get('/map', function(req, res) {
     const map = fs.readFileSync("maptest.txt", 'utf8');
@@ -26,104 +27,110 @@ app.get('/noisejs', function(req, res) {
 
 //mapconstructor
 app.get('/snoise', function(req, res) {
-    const size = req.query.size;
-    const sizeOfChuncks = 25
-    const squares = (size*size)/(sizeOfChuncks*sizeOfChuncks)
-    let img3 = []
-    for(num=0; num < size; ++num){
-        img3.push([])
-    }
-    for(i = 0; i < squares; ++i){
-        //console.log(i);
-        gen = new SimplexNoise()
-        const img = genee(sizeOfChuncks, sizeOfChuncks)
-        const img2 = map(img,(cur, px,py,ix,iy)=>{
-                let v = octave_noise(ix,iy,8)
-                return v
-        })
-        for(lis = 0; lis < img2.length; ++lis){
-            y = parseInt(size/(lis*i))
-            let row = (parseInt(i/(size/sizeOfChuncks))*(sizeOfChuncks))+lis;
-            for(value = 0; value<img2[lis].length; ++value){
-                img3[row].push(img2[lis][value])
-            }
+    if(cashedmap.length == 0){
+        const size = req.query.size;
+        const sizeOfChuncks = 25
+        const squares = (size*size)/(sizeOfChuncks*sizeOfChuncks)
+        let img3 = []
+        for(num=0; num < size; ++num){
+            img3.push([])
         }
-    }
-
-    img3 = deviate(img3, 20)
-    img3 = averageOut(img3, 1, size, 2)
-
-    function genee(width, height) {
-            const rows = [];
-            for (let y = 0; y < height; y++) {
-                const row = [];
-                for(let x=0; x<width; x++) {
-                    row[x] = 0;
-                }
-                rows[y] = row;
-            }
-            return rows;
-    }
-
-    function octave_noise(nx,ny,octaves) {
-        let val = 0;
-        let freq = 1;
-        let max = 0;
-        let amp = 1;
-        for(let i=0; i<octaves; i++) {
-            gene = gen.noise2D(nx, ny) * 0.5 + 0.5;
-            val += parseFloat(gene)*amp;
-            max += amp;
-            amp /= 2;
-            freq  *= 2;
-        }
-        return val/max;
-    }
-
-    function map(data,cb) {
-        const width = data[0].length;
-        const height = data.length;
-        return data.map((row,py)=>{
-            return row.map((val,px) => {
-                const nx = px/width, ny = py/height;
-                return cb(val,px,py,nx,ny);
+        for(i = 0; i < squares; ++i){
+            //console.log(i);
+            gen = new SimplexNoise()
+            const img = genee(sizeOfChuncks, sizeOfChuncks)
+            const img2 = map(img,(cur, px,py,ix,iy)=>{
+                    let v = octave_noise(ix,iy,8)
+                    return v
             })
-        });
-    }
-    function averageOut(img, times, size, radii){
-        for(i = 0;i<times; ++i){
-            for(y = 0;y<img.length; ++y){
-                for(x = 0;x<img[y].length; ++x){
-                    let total = 0;
-                    let values = 0;
-                    for(y1 = -1*radii;y1<radii+1; ++y1){
-                        for(x1 = -1*radii;x1<radii+1; ++x1){
-                            if (x+x1 >= 0 && x+x1 < size && y+y1 >= 0 && y+y1 < size){
-                                total += img[x+x1][y+y1]
-                                values +=1
+            for(lis = 0; lis < img2.length; ++lis){
+                y = parseInt(size/(lis*i))
+                let row = (parseInt(i/(size/sizeOfChuncks))*(sizeOfChuncks))+lis;
+                for(value = 0; value<img2[lis].length; ++value){
+                    img3[row].push(img2[lis][value])
+                }
+            }
+        }
+
+        img3 = deviate(img3, 20)
+        img3 = averageOut(img3, 1, size, 2)
+
+        function genee(width, height) {
+                const rows = [];
+                for (let y = 0; y < height; y++) {
+                    const row = [];
+                    for(let x=0; x<width; x++) {
+                        row[x] = 0;
+                    }
+                    rows[y] = row;
+                }
+                return rows;
+        }
+
+        function octave_noise(nx,ny,octaves) {
+            let val = 0;
+            let freq = 1;
+            let max = 0;
+            let amp = 1;
+            for(let i=0; i<octaves; i++) {
+                gene = gen.noise2D(nx, ny) * 0.5 + 0.5;
+                val += parseFloat(gene)*amp;
+                max += amp;
+                amp /= 2;
+                freq  *= 2;
+            }
+            return val/max;
+        }
+
+        function map(data,cb) {
+            const width = data[0].length;
+            const height = data.length;
+            return data.map((row,py)=>{
+                return row.map((val,px) => {
+                    const nx = px/width, ny = py/height;
+                    return cb(val,px,py,nx,ny);
+                })
+            });
+        }
+        function averageOut(img, times, size, radii){
+            for(i = 0;i<times; ++i){
+                for(y = 0;y<img.length; ++y){
+                    for(x = 0;x<img[y].length; ++x){
+                        let total = 0;
+                        let values = 0;
+                        for(y1 = -1*radii;y1<radii+1; ++y1){
+                            for(x1 = -1*radii;x1<radii+1; ++x1){
+                                if (x+x1 >= 0 && x+x1 < size && y+y1 >= 0 && y+y1 < size){
+                                    total += img[x+x1][y+y1]
+                                    values +=1
+                                }
                             }
                         }
+                        img[y][x] = total/values
                     }
-                    img[y][x] = total/values
                 }
             }
+
+            return img;
         }
 
-        return img;
-    }
-
-    function deviate(img, times){
-        for(i = 0;i<times; ++i){
-            for(y = 0;y<img.length; ++y){
-                for(x = 0;x<img[y].length; ++x){
-                    img[y][x] = img[y][x] + ((Math.random()-0.5)/20)*1.5
+        function deviate(img, times){
+            for(i = 0;i<times; ++i){
+                for(y = 0;y<img.length; ++y){
+                    for(x = 0;x<img[y].length; ++x){
+                        img[y][x] = img[y][x] + ((Math.random()-0.5)/20)*1.5
+                    }
                 }
             }
-        }
 
-        return img;
+            return img;
+        }
+        cashedmap = img3
+        res.json(img3);
     }
-    res.json(img3);
+    else{
+        res.json(cashedmap);
+    }
 });
 
 app.listen(3000, function() {
